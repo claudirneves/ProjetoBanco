3bbd8ad819f56665e3c7a4cbbc9c294d58b177b2
aluno <aluno@MSCGRESL04003.estacio.acad>
2017-10-19 21:51:23 -0300

    Conexão com o Banco de Dados - Atividades

---
+++ ProjetoBanco/src/main/java/br/estacio/programacao/banco/atividade/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao.banco.atividade;
+
+/**
+ *
+ * @author aluno
+ */
+public class Usuario {
+    private int id_usuario;
+    private String nome;
+    private String sobrenome;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+}
